7911d5a61120f94ebb6fca60a07733943913396d
Prathibha M. Prasanna <165554289+prathibhaprasanna@users.noreply.github.com>
2026-06-15 14:34:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
+# Import python packages
 import streamlit as st
 import pandas as pd
 from snowflake.snowpark.functions import col
 import requests
 
-st.title("Smoothie App - Debug Mode")
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie!")
+
+name_on_order = st.text_input('Name on Smoothie: ')
+st.write('The name on your smoothie will be:', name_on_order)
 
 cnx = st.connection('snowflake')
 session = cnx.session()
@@ -11,7 +17,25 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 
-st.write("Columns in DataFrame:", pd_df.columns.tolist())
-st.write("First 5 rows:")
-st.dataframe(pd_df.head()))
+ingredients_list = st.multiselect('Choose up to 5 ingredients: ', pd_df['FRUIT_NAME'].tolist())
 
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+
+    time_to_insert = st.button('Submit order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
